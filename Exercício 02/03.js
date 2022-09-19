703d6ea7124a2b323cd7dab83e433bd7af382391
Israel Benvindo <75487136+is-benvind@users.noreply.github.com>
2022-09-18 23:14:38 -0300

    Add files via upload

---
+++ Exercício 02/03.js
@@ -0,0 +1,6 @@
+const num = "10";
+const operacao = num + 10; //1010
+
+// No exemplo acima, o programa, feito em JavaScript (uma linguagem dinamicamente tipada), fez uma conversão 
+// implícita do valor 10, de número para string, para então concatenar com a string "10", o que gerou um resultado 
+// errado para a soma. 
---
+++ Exercício 02/07.ts
@@ -0,0 +1,5 @@
+const nome : string = "Ely";
+const numero : number = 120.56;
+const linguagem : string = "TypeScript";
+
+console.log(`${nome} \nMy payment time is ${numero} \nand \nmy preffered language is linguagem`)
